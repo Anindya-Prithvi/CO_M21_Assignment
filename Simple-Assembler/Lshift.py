6adc20b3b1372771c12ec7469b382f489398e9ea
g-nerix <61135649+g-nerix@users.noreply.github.com>
2021-08-09 23:10:04 +0530

    Update Lshift.py

--- Simple-Assembler/Lshift.py
+++ Simple-Assembler/Lshift.py
@@ -1,7 +1,9 @@
 def Lshift(str_in):
+
     arr = str_in.split()
     output = '01001' 
     err = ''
+
     register = {'R0':'000',
                 'R1':'001',
                 'R2':'010',
@@ -11,29 +13,40 @@ def Lshift(str_in):
                 'R6':'110',
                 # 'R7':'111';
                 }
+
     rlist = ['R0','R1','R2','R3','R4','R5','R6']
+
     if len(arr) == 3:
         if arr[1] in rlist:
             output = output + register.get(arr[1])
         else:
             err = "ERROR:INVALID REGISTER CODE"
             return err
+        
         imm = arr[2]
+        
         if imm[0] == "$":
+            
             imm = int(imm[1:len(imm)])
+            
             if imm in range(256):
                 imm_bin = str(bin(imm))
                 output = output + imm_bin[2:len(imm_bin)]
             else:
                 err = "ERROR:IMMIDIATE OUT OF BOUND"
                 return err
+
         else:
             err = "ERROR:INVALID IMMIDIATE INPUT"
             return err
+        
         return output
+    
     else:
         err = "ERROR:INVALID NUMBER OF ARGUMENTS"
         return err
 
-s_in = "ls R10 $10"
-print(Lshift(s_in))
+# TEST
+# s_in = "ls R1 $10"
+# print(Lshift(s_in))
+# output = 01001 001 1010
