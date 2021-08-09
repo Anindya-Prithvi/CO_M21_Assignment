bab35d6db6db76b73174355ba279e8ab8477556a
g-nerix <61135649+g-nerix@users.noreply.github.com>
2021-08-09 23:10:15 +0530

    Update Rshift.py

---
+++ Simple-Assembler/Rshift.py
@@ -0,0 +1,52 @@
+def Rshift(str_in):
+    
+    arr = str_in.split()
+    output = '01000' 
+    err = ''
+
+    register = {'R0':'000',
+                'R1':'001',
+                'R2':'010',
+                'R3':'011',
+                'R4':'100',
+                'R5':'101',
+                'R6':'110',
+                # 'R7':'111';
+                }
+
+    rlist = ['R0','R1','R2','R3','R4','R5','R6']
+
+    if len(arr) == 3:
+        if arr[1] in rlist:
+            output = output + register.get(arr[1])
+        else:
+            err = "ERROR:INVALID REGISTER CODE"
+            return err
+        
+        imm = arr[2]
+        
+        if imm[0] == "$":
+            
+            imm = int(imm[1:len(imm)])
+            
+            if imm in range(256):
+                imm_bin = str(bin(imm))
+                output = output + imm_bin[2:len(imm_bin)]
+            else:
+                err = "ERROR:IMMIDIATE OUT OF BOUND"
+                return err
+
+        else:
+            err = "ERROR:INVALID IMMIDIATE INPUT"
+            return err
+        
+        return output
+    
+    else:
+        err = "ERROR:INVALID NUMBER OF ARGUMENTS"
+        return err
+
+# TEST
+# s_in = "rs R1 $10"
+# print(Rshift(s_in))
+# output = 01000 001 1010
