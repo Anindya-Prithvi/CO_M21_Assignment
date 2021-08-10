f8f9b79208a1e440cd97cd1f040dd200290cf5be
Darkcoder02 <82827159+Darkcoder02@users.noreply.github.com>
2021-08-10 23:40:45 +0530

    out of range error fixed

--- Simple-Assembler/ay_in/mul.py
+++ Simple-Assembler/ay_in/mul.py
@@ -8,6 +8,8 @@ def mul (mul_instruction):
                 'R6':'110',
                 }
     list=mul_instruction.split(" ")
+    if (len(list)<4):
+        return "ERROR:INCOMPLETE INSTRUCTION"
     if list[0]!="mul":
         return "ERROR:ILLEGAL ARGUMENT"
     else:
